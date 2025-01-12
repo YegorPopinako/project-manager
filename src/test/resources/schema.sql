aa3f3a094bb4f48b6b161fc822607b134f10da21
YegorPopinako <yegor5x@gmail.com>
2025-01-12 23:34:27 +0200

    test: configured h2 database for tests

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,33 @@
+drop table if exists `project` cascade;
+drop table if exists `task` cascade;
+drop table if exists `user` cascade;
+
+CREATE TABLE `project` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `description` varchar(255) NOT NULL,
+                           `title` varchar(255) NOT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `task` (
+                        `estimate` int DEFAULT NULL,
+                        `end_point` datetime(6) DEFAULT NULL,
+                        `id` bigint NOT NULL AUTO_INCREMENT,
+                        `project_id` bigint DEFAULT NULL,
+                        `start_point` datetime(6) DEFAULT NULL,
+                        `updated_at` datetime(6) DEFAULT NULL,
+                        `description` varchar(255) DEFAULT NULL,
+                        `status` varchar(255) DEFAULT NULL,
+                        `title` varchar(255) DEFAULT NULL,
+                        PRIMARY KEY (`id`),
+                        KEY `FKk8qrwowg31kx7hp93sru1pdqa` (`project_id`),
+                        CONSTRAINT `FKk8qrwowg31kx7hp93sru1pdqa` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `user` (
+                        `id` bigint NOT NULL AUTO_INCREMENT,
+                        `password` varchar(255) DEFAULT NULL,
+                        `role` varchar(255) DEFAULT NULL,
+                        `username` varchar(255) DEFAULT NULL,
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
