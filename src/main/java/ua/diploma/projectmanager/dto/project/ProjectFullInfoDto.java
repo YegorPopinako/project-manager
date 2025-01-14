03f970dc4edc904a273bfd15ab4915644154e27a
YegorPopinako <yegor5x@gmail.com>
2025-01-14 13:38:12 +0200

    feat(dto): add FullInfoProjectDto for detailed project information

---
+++ src/main/java/ua/diploma/projectmanager/dto/project/ProjectFullInfoDto.java
@@ -0,0 +1,13 @@
+package ua.diploma.projectmanager.dto.project;
+
+import lombok.Data;
+
+@Data
+public class ProjectFullInfoDto {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+}
