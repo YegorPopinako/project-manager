939c9069822893206412b71bd1a8aee4ee8dcaca
YegorPopinako <yegor5x@gmail.com>
2025-01-25 01:13:04 +0200

    feat(entity): update User entity

--- src/main/java/ua/diploma/projectmanager/model/User.java
+++ src/main/java/ua/diploma/projectmanager/model/User.java
@@ -1,22 +1,67 @@
 package ua.diploma.projectmanager.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ua.diploma.projectmanager.security.login.Role;
+
+import java.util.Collection;
+import java.util.Set;
 
 @Entity
-@Data
-public class User {
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(exclude = "roles")
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
-    private String username;
+    private String displayName;
 
+    @Size(max = 128)
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Size(min = 5, max = 128)
     private String password;
 
-    private String role;
+    @Size(min = 2, max = 32)
+    private String firstName;
+
+    @Size(max = 32)
+    private String lastName;
+
+    @CollectionTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "role"}, name = "uk_user_role")
+    )
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @PostPersist
+    private void calculateDisplayName() {
+        this.displayName = firstName + " " + lastName;
+    }
 }
