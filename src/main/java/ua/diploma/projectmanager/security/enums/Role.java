22f33548846cdd7e7a8c8a1284505b71924e116e
YegorPopinako <yegor5x@gmail.com>
2025-03-21 19:13:37 +0200

    refactor: change roles

--- src/main/java/ua/diploma/projectmanager/security/enums/Role.java
+++ src/main/java/ua/diploma/projectmanager/security/enums/Role.java
@@ -4,9 +4,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
     USER,
-    DEV,
-    ADMIN,
-    MANAGER;
+    ADMIN;
 
     @Override
     public String getAuthority() {
